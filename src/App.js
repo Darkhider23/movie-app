bb94941559cf335aac67f2d1ce8f1e27cec87555
Farchi <farcas683@gmail.com>
2023-02-28 19:19:19 +0200

    working api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState} from 'react';
+import axios from 'axios';
+import Moviepage from './components/movie-page/movie-page';
 
 function App() {
+  const [moviedata,setmovieData]=useState([]);
+  const [searchQuery,setSearchQuery] = useState('');
+
+
+   const handleOnSearchChange = (event)=>{
+     setSearchQuery(event.target.value);
+   }
+
+  const handleOnSearchSubmit = (event)=>{
+    if(event.keyCode === 13){
+      const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/find',
+        params: {q: searchQuery},
+        headers: {
+          'X-RapidAPI-Key': 'a8003c93e5mshcd18d73914e323dp1cb017jsn34c9d2a8d394',
+          'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+      };
+    
+      
+      axios.request(options).then(async (response) =>{
+      
+          const movdata = await response.data.results;
+          console.log(movdata);
+          movdata.map((data,index)=>(
+            (index == 0)?
+            setmovieData(data)
+            :
+            null
+          ))
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='searchBar'>
+        <div className='inputbutton'>
+        <input type = "text" value={searchQuery} onChange= {handleOnSearchChange} onKeyDown = {handleOnSearchSubmit}/>
+        <button onClick={handleOnSearchChange} type='submit'>Search</button>
+        </div>
+        </div>
+      {moviedata && <Moviepage data={moviedata}/>}
     </div>
+
+        // { {moviedata.map((data,index)=>(
+        //   (index === 0) ?
+        //   <div>{data.title}</div>
+        //   :
+        //   null
+        // ))} }
+
   );
 }
 
