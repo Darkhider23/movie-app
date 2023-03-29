b4ca9d9b4625a933a01d42b67e53ff416a191963
Farchi <farcas683@gmail.com>
2023-03-29 19:08:02 +0300

    almost done image for cast but ran out of api call

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import './App.css';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Moviepage from './components/movie-page/movie-page';
-
+import logo from './assets/logo.png'
 function App() {
-  const [moviedata,setmovieData]=useState([]);
-  const [searchQuery,setSearchQuery] = useState('');
+  const [moviedata, setmovieData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
 
-   const handleOnSearchChange = (event)=>{
-     setSearchQuery(event.target.value);
-   }
+  const handleOnSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  }
 
-  const handleOnSearchSubmit = (event)=>{
-    if(event.keyCode === 13){
+  const handleOnSearchSubmit = (event) => {
+    if (event.keyCode === 13) {
       const options = {
         method: 'GET',
         url: 'https://imdb8.p.rapidapi.com/title/find',
-        params: {q: searchQuery},
+        params: { q: searchQuery },
         headers: {
           'X-RapidAPI-Key': 'a8003c93e5mshcd18d73914e323dp1cb017jsn34c9d2a8d394',
           'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
         }
       };
-    
-      
-      axios.request(options).then(async (response) =>{
-      
-          const movdata = await response.data.results;
-          console.log(movdata);
-          movdata.map((data,index)=>(
-            (index == 0)?
+
+
+      axios.request(options).then(async (response) => {
+
+        const movdata = await response.data.results;
+        console.log(movdata);
+        movdata.map((data, index) => (
+          (index === 0) ?
             setmovieData(data)
             :
             null
-          ))
+        ))
       }).catch(function (error) {
         console.error(error);
       });
@@ -43,20 +43,17 @@ function App() {
   return (
     <div>
       <div className='searchBar'>
+        <div className="logo">
+          <img src={logo} alt="" />
+          <h1>Movies Holmes</h1>
+        </div>
         <div className='inputbutton'>
-        <input type = "text" value={searchQuery} onChange= {handleOnSearchChange} onKeyDown = {handleOnSearchSubmit}/>
-        <button onClick={handleOnSearchChange} type='submit'>Search</button>
+          <input type="text" value={searchQuery} onChange={handleOnSearchChange} onKeyDown={handleOnSearchSubmit} />
+          <button onClick={handleOnSearchChange} type='submit'>Search</button>
         </div>
-        </div>
-      {moviedata && <Moviepage data={moviedata}/>}
+      </div>
+      {moviedata && <Moviepage data={moviedata} />}
     </div>
-
-        // { {moviedata.map((data,index)=>(
-        //   (index === 0) ?
-        //   <div>{data.title}</div>
-        //   :
-        //   null
-        // ))} }
 
   );
 }
--- src/components/movie-page/movie-page.js
+++ src/components/movie-page/movie-page.js
@@ -3,61 +3,174 @@ import React, { useEffect } from "react"
 import axios from "axios";
 import { useState } from "react";
 const Moviepage = ({ data }) => {
-    const [movieRating,setmovieRating] = useState();
-    useEffect(()=>{
-        let titleid='';
-        if(data.id){
-        titleid = data.id;
-        console.log(typeof titleid);
-        console.log(typeof data.id);
-        titleid = titleid.slice(7,titleid.length-1);
-        console.log(titleid);
-    }
-    else{
-        console.log("not id");
-    }
-    const options = {
-        method: 'GET',
-        url: 'https://imdb8.p.rapidapi.com/title/get-ratings',
-        params: {tconst: titleid},
-        headers: {
-          'X-RapidAPI-Key': 'a8003c93e5mshcd18d73914e323dp1cb017jsn34c9d2a8d394',
-          'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    const [movieRating, setmovieRating] = useState();
+    const [plot, setPlot] = useState();
+    const [genre, setGenre] = useState([]);
+    const [actorimg, setActorImg] = useState();
+    const [buttontext, setButtonText] = useState('Add to my watchlist');
+    useEffect(() => {
+        setButtonText('Add to my watchlist');
+        let titleid = '';
+        if (data.id) {
+            titleid = data.id;
+            titleid = titleid.slice(7, titleid.length - 1);            
+            //rating call
+            {
+                const rating_options = {
+                    method: 'GET',
+                    url: 'https://imdb8.p.rapidapi.com/title/get-ratings',
+                    params: { tconst: titleid },
+                    headers: {
+                        'X-RapidAPI-Key': 'a8003c93e5mshcd18d73914e323dp1cb017jsn34c9d2a8d394',
+                        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+                    }
+                };
+
+                axios.request(rating_options).then(async (response) => {
+                    const rat = await response.data;
+                    console.log(rat.rating);
+                    setmovieRating(rat.rating);
+
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            }
+            //plot call
+            {
+                const plot_options = {
+                    method: 'GET',
+                    url: 'https://imdb8.p.rapidapi.com/title/get-plots',
+                    params: { tconst: titleid },
+                    headers: {
+                        'X-RapidAPI-Key': '6eb5ee05aamshf53aa2c30883a0bp14d696jsnd477cca6a087',
+                        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+                    }
+                };
+
+                axios.request(plot_options).then(async (response) => {
+                    const plot = await response.data;
+                    
+                    setPlot(plot.plots[0].text);
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            }
+            //genre call
+            {
+                const genre_options = {
+                    method: 'GET',
+                    url: 'https://imdb8.p.rapidapi.com/title/get-genres',
+                    params: { tconst: titleid },
+                    headers: {
+                        'X-RapidAPI-Key': '6eb5ee05aamshf53aa2c30883a0bp14d696jsnd477cca6a087',
+                        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+                    }
+                };
+
+                axios.request(genre_options).then(async (response) => {
+                    const genre = await response.data;
+                    setGenre(genre);
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            }
         }
-      };
-      
-      axios.request(options).then(async (response)=> {
-          const rat = await response.data;
-          console.log(rat.rating);
-          setmovieRating(rat.rating);
+        else {
+        }
 
-      }).catch(function (error) {
-          console.error(error);
-      });
-    },[]);
+    }, [data.id]);
+
+    // function image_actor(actorid){
+    //         actorid.slice(6, actorid.length - 1);
+    //         const actor_image_options = {
+    //             method: 'GET',
+    //             url: 'https://imdb8.p.rapidapi.com/actors/get-bio',
+    //             params: { nconst: actorid },
+    //             headers: {
+    //                 'X-RapidAPI-Key': '6eb5ee05aamshf53aa2c30883a0bp14d696jsnd477cca6a087',
+    //                 'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    //             }
+    //         };
+
+    //         axios.request(actor_image_options).then(async (response) => {
+    //             const actorimg = await response.data;
+    //             setActorImg(actorimg.image.url);
+    //             // console.log(actorimg);
+    //         }).catch(function (error) {
+    //             console.error(error);
+    //         });
+    // }
+
+
+    function buttonClick() {
+        var checkmark = '✔';
+        if (buttontext === 'Add to my watchlist') {
+            setButtonText(checkmark);
+        }
+        else {
+            setButtonText('Add to my watchlist');
+        }
+    }
     return (
-        
-        <div className="page-container">
-            <div className="top-left">
-                <div className="title">{data.title}</div>
-                <div className="top-left-sub">
-                    <span>-</span>
-                    <span className="type">{data.titleType}</span>
-                    <span>-</span>
-                    <span className="year">{data.seriesStartYear}-{data.seriesEndYear}</span>
-                    <span>-</span>
-                    <span className="average-time">{data.runningTimeInMinutes}m</span>
-                    <span>-</span>
-                </div>
+        <>
+            <div className="page-container">
+                {data.length === 0 ? <div></div> :
+                    <>
+                        <div className="top-details">
+                            <div className="top-left">
+                                <img src={data.image.url} alt="" />
+                            </div>
+                            <div className="top-right">
+                                <div className="title">{data.title}</div>
+                                <div className="top-rating">
+                                    <div className="value">	&#11088;{movieRating}
+                                    </div>
+                                </div>
+                                <div className="top-details">
 
+                                    {data.titleType ? <span className="type">{data.titleType}</span> : null}
+
+                                    {data.seriesStartYear ? <span className="year">{data.seriesStartYear}{data.seriesEndYear ? <span className="year">-{data.seriesEndYear}</span> : null}</span> : null}
+
+                                    {data.runningTimeInMinutes ? <span className="average-time">{data.runningTimeInMinutes}m</span> : null}
+                                </div>
+                                <div className="top-genre">
+                                    {genre[0] ? <h3>{genre[0]}</h3> : null}
+                                    {genre[1] ? <h3>{genre[1]}</h3> : null}
+                                    {genre[2] ? <h3>{genre[2]}</h3> : null}
+                                </div>
+                                <button id="myAddbutton" onClick={buttonClick}>
+                                    {buttontext}
+                                </button>
+                            </div>
+                        </div>
+                        <div className="middle">
+                            <div className="plot-container">
+                                <h1>
+                                    Plot:
+                                </h1>
+                                <p>{plot}</p>
+                            </div>
+                            <div className="cast-container">
+                                <h1>Cast</h1>
+                                <p>
+                                    {/* {image_actor(data.principals[0].id)}
+                                    <img src={actorimg} alt="" height={100}/>
+                                    {data.principals[0].name ? <span>{data.principals[0].name} <br/> {data.principals[0].roles[0].character}</span> : null}
+
+                                    
+                                    {data.principals[1].name ? <span>{data.principals[1].name} <br/> {data.principals[0].roles[0].character}</span> : null}
+
+                                    
+                                    {data.principals[2].name ? <span>{data.principals[2].name} <br/> {data.principals[0].roles[0].character}</span> : null} */}
+                                </p>
+
+                            </div>
+                        </div>
+                    </>
+                }
             </div>
-            <div className="top-right">
-                <div className="rating">
-                    <div className="label">Rating</div>
-                    <div className="value">	&#11088;{movieRating}</div>
-                </div>
-            </div>
-        </div>
+        </>
     );
 };
 export default Moviepage
